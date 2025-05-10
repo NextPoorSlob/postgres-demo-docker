fcad4d1ddf11b9d3a4a33dd2965414f7bceffce2
Steve Gelman <sgelman@nextpoorslob.com>
2025-05-10 09:08:34 -0500

    Test of Faker

---
+++ sql/script.sql
@@ -0,0 +1,32 @@
+begin;
+
+
+
+create table if not exists employees_temp
+(
+    name       text,
+    age        int,
+    email      text,
+    department text,
+    salary     bigint
+);
+
+create table if not exists employees
+(
+    name       text,
+    age        int,
+    email      text,
+    department text,
+    salary     bigint,
+    primary key (name, email)
+    );
+
+\copy employees_temp(name, age, email, department, salary) from 'employees.csv' with delimiter ',' csv header;
+
+insert into employees (name, age, email, department, salary)
+select name, age, email, department, salary from employees_temp
+    on conflict (name, email) do nothing;
+
+drop table employees_temp;
+
+commit;
